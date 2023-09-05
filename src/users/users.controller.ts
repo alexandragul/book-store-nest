1e77bada883e7242ee7a686294172e6e04768e64
Aliaksandra Hul <alexandra.ggul@gmail.com>
2023-09-05 10:02:22 +0300

    Implement roles guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from 'src/users/users.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Roles } from 'src/auth/roles.decorator';
+import { RolesGuard } from 'src/auth/roles.guard';
 
 @Controller('users')
 export class UsersController {
@@ -13,6 +15,8 @@ export class UsersController {
   }
 
   @UseGuards(JwtAuthGuard)
+  @Roles('ADMIN')
+  @UseGuards(RolesGuard)
   @Get()
   getAll() {
     return this.usersService.getAllUsers();
