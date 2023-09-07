a624fccc094632db9605b5eefb90f2edac5f8b53
Aliaksandra Hul <alexandra.ggul@gmail.com>
2023-09-07 09:21:53 +0300

    Add role validation

--- src/users/dto/add-role.dto.ts
+++ src/users/dto/add-role.dto.ts
@@ -1,4 +1,9 @@
+import { IsNumber, IsString } from 'class-validator';
+
 export class AddRoleDto {
+  @IsString({ message: 'Has to be string' })
   readonly value: string;
+
+  @IsNumber({}, { message: 'Has to be number' })
   readonly userId: number;
 }
