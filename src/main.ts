a47a500d9df4d58c6da867bbd4cf5c52c99e734c
Aliaksandra Hul <alexandra.ggul@gmail.com>
2023-09-07 09:19:44 +0300

    Configure validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'src/app.module';
+import { ValidationPipe } from 'src/pipes/validation.pipe';
 
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(PORT, () => {
     console.log(`Server started on port = ${PORT}`);
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { IsEmail, IsString, Length } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString({ message: 'Has to be string' })
+  @IsEmail({}, { message: 'Incorrect email' })
   readonly email: string;
+
+  @IsString({ message: 'Has to be string' })
+  @Length(4, 16, { message: 'Has to be less that 16 but more that 4' })
   readonly password: string;
 }
