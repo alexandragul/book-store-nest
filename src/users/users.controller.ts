448c5172bee680274321607af0adc07898d91164
Aliaksandra Hul <alexandra.ggul@gmail.com>
2023-09-07 09:02:24 +0300

    Implement roles grant

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,6 +4,7 @@ import { UsersService } from 'src/users/users.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { Roles } from 'src/auth/roles.decorator';
 import { RolesGuard } from 'src/auth/roles.guard';
+import { AddRoleDto } from 'src/users/dto/add-role.dto';
 
 @Controller('users')
 export class UsersController {
@@ -20,5 +21,13 @@ export class UsersController {
   @Get()
   getAll() {
     return this.usersService.getAllUsers();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Roles('ADMIN')
+  @UseGuards(RolesGuard)
+  @Post('/role')
+  addRole(@Body() dto: AddRoleDto) {
+    return this.usersService.addRole(dto);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from 'src/users/users.model';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { RolesService } from 'src/roles/roles.service';
+import { AddRoleDto } from 'src/users/dto/add-role.dto';
 
 @Injectable()
 export class UsersService {
@@ -27,5 +28,17 @@ export class UsersService {
   async getUserByEmail(email: string) {
     const user = await this.userRepository.findOne({ where: { email }, include: { all: true } });
     return user;
+  }
+
+  async addRole(dto: AddRoleDto) {
+    const user = await this.userRepository.findByPk(dto.userId);
+    const role = await this.rolesService.getRoleByValue(dto.value);
+
+    if (role && user) {
+      await user.$add('role', role.id);
+      return dto;
+    }
+
+    throw new HttpException('User or role has not been found', HttpStatus.NOT_FOUND);
   }
 }
